4ce015b537410f45df4aeb7ed9b3e24a53d97aab
ofanan <ofanan@gmail.com>
2020-11-09 17:17:05 +0200

    Improved parsing of res file from either BW-based or uInerval-bsas runs (with comenting out / in several key rows).
    file:///C:/Users/ofanan/Documents/GitHub/Access_strategies_with_staleness/src/DataStore.py
    file:///C:/Users/ofanan/Documents/GitHub/Access_strategies_with_staleness/src/Keys_list_parser.py
    file:///C:/Users/ofanan/Documents/GitHub/Access_strategies_with_staleness/src/Res_file_parser.py
    file:///C:/Users/ofanan/Documents/GitHub/Access_strategies_with_staleness/src/Res_file_parser_runner.py
    file:///C:/Users/ofanan/Documents/GitHub/Access_strategies_with_staleness/src/runner.py

--- src/DataStore.py
+++ src/DataStore.py
@@ -48,7 +48,7 @@ class DataStore (object):
         self.verbose                = verbose #if self.ID==0 else 0
         self.ins_cnt                = np.uint32 (0)
         self.num_of_fpr_fnr_updates = int (0)
-        self.num_of_insertions_between_estimations = np.uint8 (max (self.cache_size / 1000, 1))
+        self.num_of_insertions_between_estimations = np.uint8 (max (self.cache_size / 500, 1))
         self.uInterval = uInterval
         if (self.verbose == 3):
             self.debug_file = open ("../res/fna.txt", "w")
@@ -113,16 +113,16 @@ class DataStore (object):
     def send_update (self, check_delta_th = False):
             
         self.num_of_updates += 1
-        self.fnr = 0 # Immediately after sending an update, the expected fnr is 0
-        updated_sbf = self.updated_indicator.gen_SimpleBloomFilter ()
         if (check_delta_th):
+            updated_sbf = self.updated_indicator.gen_SimpleBloomFilter ()
             Delta = [sum (np.bitwise_and (~updated_sbf.array, self.stale_indicator.array)), sum (np.bitwise_and (updated_sbf.array, ~self.stale_indicator.array))]
             if (sum (Delta) < self.delta_th):
                 print ('sum_Delta = ', sum (Delta), 'delta_th = ', self.delta_th, 'Sending delta updates is cheaper\n')
                 exit ()
-        self.stale_indicator = self.updated_indicator.gen_SimpleBloomFilter ()
-        B1_st = sum (self.stale_indicator.array)    # Num of bits set in the stale indicator
+        self.stale_indicator = self.updated_indicator.gen_SimpleBloomFilter () # "stale_indicator" is the snapshot of the current state of the ind', until the next update
+        B1_st = sum (self.stale_indicator.array)    # Num of bits set in the updated indicator
         self.fpr = pow ( B1_st / self.BF_size, self.num_of_hashes)
+        self.fnr = 0 # Immediately after sending an update, the expected fnr is 0
 #         print ('d.fpr = {:.4f}, B1_st = {:.0f}, calc.fpr = {:.4f}' .format (self.designed_fpr, B1_st, pow ( B1_st / self.BF_size, self.num_of_hashes)))
 #         self.fpr = self.designed_fpr # Immediately after sending an update, the expected fpr is the inherent fpr
         
--- src/Res_file_parser.py
+++ src/Res_file_parser.py
@@ -8,43 +8,11 @@ num_of_DSs_idx  = 4
 kloc_idx        = 5
 missp_idx       = 6
 bw_idx          = 7
-alg_idx         = 8
-num_of_fields   = 9
+uInterval_idx   = 8
+alg_idx         = 9
+num_of_fields   = alg_idx + 1
 
-trace       = "gradle"
-cache_size  = "40"
-bpe         = "14"
-num_of_req  = "500"
-num_of_DSs  = "3"
-kloc        = "1"
-missp       = "100" 
-bw          = "10"
-
-class Res_file_parser (object):
-
-    
-#     def __init__ (self, input_file_name):
-#         self.input_file  = open ("../res/" + input_file_name,  "r")
-#         self.output_file = open ("../res/" + input_file_name.split(".")[0] + ".dat", "a")  
-    
-# def is_requested_entry (line, trace = "gradle", cache_size  = "10K", bpe = "14", num_of_req  = "750", 
-#                    num_of_DSs  = "3", kloc = "1", missp = "100", bw = "10", alg_mode = "Opt"):
-#     splitted_line = line.split (".")
-#     
-#     if len (splitted_line) < num_of_fields:
-#         print ("encountered a format error")
-#         return False
-#     if (self.settings[trace_idx]        ==              trace       and 
-#         self.settings[cache_size_idx]   == 'C'        + cache_size  and 
-#         self.settings[bpe_idx]          == 'bpe'      +  bpe        and
-#         self.settings[num_of_req_idx]   == num_of_req + 'Kreq'      and
-#         self.settings[num_of_DSs_idx]   == num_of_DSs + 'DSs'       and 
-#         self.settings[kloc_idx]         == 'Kloc'     + kloc        and 
-#         self.settings[missp_idx]        == 'M'        + missp       and
-#         self.settings[bw_idx]           == 'B'        + bw          and 
-#         self.settings[alg_idx].split(" ")[0] ==         alg_mode):
-#         return True
-#     return False
+class Res_file_parser (object):  
 
     def parse_line (self, line):
         splitted_line = line.split ("|")
@@ -67,26 +35,27 @@ class Res_file_parser (object):
             "Kloc"       : int (splitted_line[kloc_idx].split("Kloc")[1]),
             "missp"      : int (splitted_line[missp_idx].split("M")[1]),
             "bw"         : int(splitted_line[bw_idx].split('B')[1]), 
+             "uInterval"  : int(splitted_line[uInterval_idx].split('U')[1]), 
             "alg_mode"   : splitted_line[alg_idx].split(" ")[0],
             "cost"       : cost
             }
 
     def print_tikz_line (self, list_of_dict, key_to_sort, legend_entry):
         for dict in sorted (list_of_dict, key = lambda i: i[key_to_sort]):
-            printf (self.output_file, '({:.0f} ,{:.04f})' .format (dict['bw'], dict['cost']))
+            printf (self.output_file, '({:.0f} ,{:.04f})' .format (dict[key_to_sort], dict['cost']))
         printf (self.output_file, '\n};\\addlegendentry {')
         printf (self.output_file, legend_entry)
         printf (self.output_file, '}\n\n')
         
         
 
-    def print_to_tikz (self, alg_mode):
+    def print_to_tikz (self, key_to_sort, alg_mode):
         if (alg_mode == 'FNA'):
             printf (self.output_file, '\\addplot[color=black,     mark=triangle,     width = \plotwidth] coordinates {\n')
-            self.print_tikz_line (self.list_of_dicts_FNA, 'bw', 'FNA')
+            self.print_tikz_line (self.list_of_dicts_FNA, key_to_sort, 'FNA')
         else:
             printf (self.output_file, '\\addplot[color=red,          mark=o,                 width = \plotwidth] coordinates {\n')
-            self.print_tikz_line (self.list_of_dicts_FNO, 'bw', 'FNO')             
+            self.print_tikz_line (self.list_of_dicts_FNO, key_to_sort, 'FNO')             
 
     def parse_file (self, input_file_name):
     
@@ -96,9 +65,6 @@ class Res_file_parser (object):
         lines = (line for line in lines if line)       # Discard blank lines
         self.list_of_dicts_FNA = []
         self.list_of_dicts_FNO = []
-        
-        # BWs = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200]    
-        # alg_modes = ["FNA"]
         
         for line in lines:
         
@@ -114,8 +80,8 @@ class Res_file_parser (object):
                 self.list_of_dicts_FNO.append(self.dict)
                 
         
-        self.print_to_tikz ('FNA')
-        self.print_to_tikz ('FNO')
+        self.print_to_tikz ('uInterval', 'FNA')
+        self.print_to_tikz ('uInterval', 'FNO')
  
         self.input_file.close
         
