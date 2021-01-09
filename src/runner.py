32ce7ecee12dae52fdd83aa679a78fe3094451f3
ofanan <ofanan@gmail.com>
2021-01-09 23:08:03 +0200

    a
    file:///C:/Users/ofanan/Documents/Github/Access_strategies_with_staleness/src/runner.py

--- src/runner.py
+++ src/runner.py
@@ -34,18 +34,18 @@ def run_tbl_sim (trace_file_name, use_homo_DS_cost = False):
     output_file         = open ("../res/tbl.res", "a")
     
     print("now = ", datetime.now(), 'running tbl sim')
-    for missp in [50, 500]:
-        for alg_mode in [sim.ALG_PGM_FNO]:
+    for missp in [50, 100, 500]:
+        for alg_mode in [sim.ALG_PGM_FNA_MR1_BY_ANALYSIS]:
             tic()
             sm = sim.Simulator(output_file, trace_file_name, alg_mode, requests, DS_cost, uInterval = uInterval, missp = missp)
             sm.run_simulator()
             toc()
-    alg_mode = sim.ALG_OPT
-    missp = 50
-    tic()
-    sm = sim.Simulator(output_file, trace_file_name, alg_mode, requests, DS_cost, uInterval = uInterval)
-    sm.run_simulator()
-    toc()
+#     alg_mode = sim.ALG_OPT
+#     missp = 50
+#     tic()
+#     sm = sim.Simulator(output_file, trace_file_name, alg_mode, requests, DS_cost, uInterval = uInterval)
+#     sm.run_simulator()
+#     toc()
 
 def run_uInterval_sim (trace_file_name, use_homo_DS_cost = False):
     """
@@ -125,7 +125,7 @@ def run_bpe_sim (trace_file_name, use_homo_DS_cost = False):
     If the input parameter "homo" is true, the access costs are uniform 1, and the miss penalty is 300/7. 
     Else, the access costs are 1, 2, 4, and the miss penalty is 100.
     """
-    max_num_of_req      = 100000#0 # Shorten the num of requests for debugging / shorter runs
+    max_num_of_req      = 1000000 # Shorten the num of requests for debugging / shorter runs
     num_of_DSs          = 3
     requests            = gen_requests (trace_file_name, max_num_of_req)
     trace_file_name     = trace_file_name.split("/")[0]
@@ -199,11 +199,13 @@ def run_k_loc_sim (trace_file_name, use_homo_DS_cost = True):
             DS_cost = calc_DS_cost (num_of_DSs, use_homo_DS_cost)            
             missp    = 50 * np.average (DS_cost)
      
-            for alg_mode in [sim.ALG_PGM_FNA_MR1_BY_HIST]: 
+            for alg_mode in [sim.ALG_PGM_FNA_MR1_BY_ANALYSIS]: 
+            # for alg_mode in [sim.ALG_PGM_FNO]: 
+            # for alg_mode in [sim.ALG_OPT]: 
                         
                 print("now = ", datetime.now(), 'running k_loc sim')
                 tic()
-                sm = sim.Simulator(output_file, trace_file_name, alg_mode, requests, DS_cost, uInterval = uInterval, 
+                sm = sim.Simulator(output_file, trace_file_name, alg_mode, requests, DS_cost, uInterval = uInterval, k_loc = k_loc, 
                                    use_given_loc_per_item = False)
                 sm.run_simulator()
                 toc()
@@ -230,19 +232,19 @@ def calc_opt_service_cost (accs_cost, comp_miss_cnt, missp, num_of_req):
     print ('Opt service cost is ', (accs_cost + comp_miss_cnt * missp) / num_of_req)
 
 
-# trace_file_name     = 'wiki/wiki.1190448987_4300K_3DSs.csv'
+trace_file_name     = 'wiki/wiki.1190448987_4300K_3DSs.csv'
 # trace_file_name     = 'gradle/gradle.build-cache_full_1000K_3DSs.csv'
-trace_file_name     = 'scarab/scarab.recs.trace.20160808T073231Z.15M_req_1000K_3DSs.csv'
+# trace_file_name     = 'scarab/scarab.recs.trace.20160808T073231Z.15M_req_1000K_3DSs.csv'
 # trace_file_name     = 'umass/storage/F2.3M_req_1000K_3DSs.csv'
 
-run_tbl_sim(trace_file_name)
+# run_tbl_sim(trace_file_name)
 # run_FN_by_staleness_sim          (trace_file_name)
 # run_bpe_sim              (trace_file_name)
 # run_uInterval_sim(trace_file_name)
 
 # run_cache_size_sim(trace_file_name)
 # run_num_of_caches_sim  (trace_file_name, use_homo_DS_cost = True)
-# run_k_loc_sim (trace_file_name)
+run_k_loc_sim (trace_file_name)
 
 
 # Opt's behavior is not depended upon parameters such as the indicaror's size, and miss penalty.
